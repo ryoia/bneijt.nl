22da891ee15b23c89ce2bfe123cf0a5947239ef4
A. Bram Neijt <bneijt@gmail.com>
2013-01-15 20:00:19 +0100

    Add ordering back, using the body for now

--- hakyll.hs
+++ hakyll.hs
@@ -4,12 +4,17 @@ module Main where
 import           Control.Applicative ((<$>))
 import           Control.Arrow       (second)
 import           Control.Monad       (forM_)
-import           Data.List           (isPrefixOf, sortBy)
+import           Data.List           (isPrefixOf, sortBy, concat)
+import           Data.Map            (findWithDefault)
 import           Data.Ord            (comparing)
 import           Data.Monoid         (mappend)
 import           Hakyll
 import           System.FilePath     (dropTrailingPathSeparator, splitPath, takeBaseName, takeDirectory)
 import           Text.Pandoc
+import Data.Maybe (fromMaybe)
+import Text.Regex (matchRegex, mkRegex)
+-- import Debug.Trace (trace)
+
 
 main :: IO ()
 main = hakyll $ do
@@ -52,17 +57,25 @@ postList pattern preprocess' = do
     posts       <- preprocess' <$> loadAll pattern
     applyTemplateList postItemTpl postContext posts
 
+selectDateLine :: String -> String
+selectDateLine body = concat (fromMaybe [""] (matchRegex (mkRegex "(<time[^>]+)") body))
+
 dropPat :: String -> Routes
 dropPat pat = gsubRoute pat (const "")
 
-chronologicalDirectory :: [Item a] -> [Item a]
+chronologicalDirectory :: [Item String] -> [Item String]
 chronologicalDirectory = sortBy $ comparing $ takeBaseName . takeDirectory . toFilePath . itemIdentifier
 
+dateFieldOfItem :: Item String -> String
+dateFieldOfItem a = selectDateLine (itemBody a)
+
+chronologicalItem :: [Item String] -> [Item String]
+chronologicalItem = sortBy $ comparing $ dateFieldOfItem
 
 --------------------------------------------------------------------------------
 -- | The reverse of 'chronological'
-recentDirectoryFirst :: [Item a] -> [Item a]
-recentDirectoryFirst = reverse . chronologicalDirectory
+recentDirectoryFirst :: [Item String] -> [Item String]
+recentDirectoryFirst = reverse . chronologicalItem
 
 postContext :: Context String
 postContext =
