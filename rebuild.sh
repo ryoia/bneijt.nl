144e07814f4832b513582aa34c1f17cbdfddb473
A. Bram Neijt <bneijt@gmail.com>
2013-11-28 21:33:14 +0100

    Compress all files that already have a .gz equivalant

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
 set -e
 cd "`dirname "$0"`"
 cabal-dev build
 dist/build/bneijt.nl/bneijt.nl clean
+
+#Recompress all files
+find static -name '*.gz' | \
+while read OUTFILE; do
+    SRCFILE="`dirname "${OUTFILE}"`/`basename "${OUTFILE}" .gz`"
+    echo "${SRCFILE}"
+    if [ -f "${SRCFILE}" ]; then
+        gzip --best -c "${SRCFILE}" > "${OUTFILE}"
+    fi
+done
+
 dist/build/bneijt.nl/bneijt.nl build
 
